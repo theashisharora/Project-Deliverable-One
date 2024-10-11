4959b1c1d9b6df5ac06a6bbb1cb19a93d0ed7353
theashisharora <arashish@sheridancollege.ca>
2024-10-11 10:13:01 -0400

    Update cardtrick

---
+++ src/ca/sheridancollege/project/WarCard.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ca.sheridancollege.project;
+
+/**
+ *
+ * @author ashis
+ */
+public abstract class WarCard extends Card{
+    
+}
---
+++ src/ca/sheridancollege/project/WarGame.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ca.sheridancollege.project;
+
+/**
+ *
+ * @author ashis
+ */
+public abstract class WarGame extends Game {
+    
+    public WarGame(String name) {
+        super(name);
+    }
+    
+}
